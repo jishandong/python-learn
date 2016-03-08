ddb2ddbcd6db7bba3306d131271c06d02d32f4e6
jishandong <1016551343@qq.com>
2016-03-08 17:21:00 +0800

    logging模块

---
+++ logging_learn.py
@@ -0,0 +1,17 @@
+import logging
+def main():
+    logging.basicConfig(
+    filename='app.log',
+    level=logging.ERROR
+    )
+    hostname='www.python.org'
+    item = 'spam'
+    filename ='data.csv'
+    mode ='r'
+    logging.critical('Host %s unknow',hostname)
+    logging.error('Could not find %r',item)
+    logging.warning('Feature is deprected')
+    logging.info('opening file %r,more=%r',filename,mode)
+    logging.debug('got here')
+if __name__=='__mian__':
+    main()
