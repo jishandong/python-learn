90c270c2635d94c5b3dd6f48e8b0ecc6fd8c71b3
jishandong <1016551343@qq.com>
2016-03-08 17:23:20 +0800

    对语句块进行性能分析和时间统计

---
+++ time_block.py
@@ -0,0 +1,16 @@
+'对语句块进行统计计时'
+from contextlib import contextmanager
+import time
+@contextmanager
+def timeblock(label):
+    start = time.perf_counter()
+    try:
+        yield
+    finally:
+        end = time.perf_counter()
+        print('{}:{}'.format(label,end - start))
+with timeblock('counting'):
+    n = 100000
+    while n > 0:
+        n -= 1
+        
